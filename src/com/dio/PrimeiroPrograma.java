8c259494b13d45ab37d457dc686a328a7626a79a
Darla <darlagalves@outlook.com>
2021-10-13 17:36:31 -0300

    Atualização Introdução as IDEs Java

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		//Criar variaveis
+		int a, b;
+		//Inicializar variaveis
+		a = b = 0;
+		//Mudar valor das variaveis
+		a = 2;
+		b= 3;
+		//Mostrar dados
+		System.out.println("Hello Word! \n"+ (a+b));
+
+	}
+
+}
